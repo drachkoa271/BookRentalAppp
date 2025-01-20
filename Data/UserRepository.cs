3f06a646b2ab2a55ecb6cbf26c386a481c7458d7
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:56:35 +0100

    napisalem kod dla pliku UserRepository.cs

--- Data/UserRepository.cs
+++ Data/UserRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace BookRentalApp.Data
+﻿using System.Collections.Generic;
+using System.Linq;
+using BookRentalApp.Models;
+
+namespace BookRentalApp.Data
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+        public User GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+        public void Add(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+        public void Update(User user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
     }
 }
