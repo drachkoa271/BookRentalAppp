70e3a4145b366581d8d7bf0c274717300a9ba3c9
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 22:40:34 +0100

    utworzono plik Deatils.cshtml

---
+++ Views/Loans/Deatils.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookRentalApp.Views.Loans
+{
+    public class DeatilsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
