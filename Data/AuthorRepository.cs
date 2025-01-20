8c035d9b1bc0f084c3b78b30e46d36b4074a7f4d
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:41:01 +0100

    napisalem kod dla pliku AuthorRepository.cs

--- Data/AuthorRepository.cs
+++ Data/AuthorRepository.cs
@@ -7,5 +7,37 @@ namespace BookRentalApp.Data
 {
     public class AuthorRepository : IAuthorRepository
     {
+        private readonly ApplicationDbContext _context;
+        public AuthorRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Author> GetAll()
+        {
+            return _context.Authors.ToList();
+        }
+        public Author GetById(int id)
+        {
+            return _context.Authors.Find(id);
+        }
+        public void Add(Author author)
+        {
+            _context.Authors.Add(author);
+            _context.SaveChanges();
+        }
+        public void Update(Author author)
+        {
+            _context.Authors.Remove(author);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var author = _context.Authors.Find(id);
+            if (author != null)
+            {
+                _context.Authors.Remove(author);
+                _context.SaveChanges();
+            }
+        }
     }
 }
