74fd5c3475f87ce85fda19345ba6ae43683681b3
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:46:13 +0100

    napisalem kod dla pliku ICategoryRepository.cs

--- Data/ICategoryRepository.cs
+++ Data/ICategoryRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace BookRentalApp.Data
+﻿using System.Collections.Generic;
+using BookRentalApp.Models;
+
+namespace BookRentalApp.Data
 {
     public interface ICategoryRepository
     {
+        IEnumerable<Category> GetAll();
+        Category GetById(int id);
+        void Add (Category category);
+        void Update (Category category);
+        void Delete (int id);
     }
 }
