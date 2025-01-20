f2bf5156345b0d9825b4f474570b329318b44b19
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 22:29:11 +0100

    napisalem kod dla pliku CategoriesController.cs

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,6 +1,94 @@
-﻿namespace BookRentalApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using BookRentalApp.Models;
+using BookRentalApp.Data;
+
+namespace BookRentalApp.Controllers
 {
-    public class CategoriesController
+    public class CategoriesController : Controller
     {
+        private readonly ICategoryRepository _repository;
+
+        public CategoriesController(ICategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _repository.GetAll();
+            return View(categories);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var category = _repository.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var category = _repository.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _repository.Update(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _repository.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _repository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
