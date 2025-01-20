8f1b131c909a148b696759e989cf5fd4681b5444
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:48:30 +0100

    napisalem kod dla pliku ILoanRepository.cs

--- Data/ILoanRepository.cs
+++ Data/ILoanRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace BookRentalApp.Data
+﻿using System.Collections.Generic;
+using BookRentalApp.Models;
+
+namespace BookRentalApp.Data
 {
     public interface ILoanRepository
     {
+        IEnumerable<Loan> GetAll();
+        Loan GetById (int id);
+        void Add (Loan loan);
+        void Update (Loan loan);
+        void Delete (int id);
     }
 }
