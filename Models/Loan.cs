e73efe3a5a54a04fc18636f534ffc8e96687c925
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:30:09 +0100

    napisalem kod dla pliku Loan.cs

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -1,6 +1,14 @@
-﻿namespace BookRentalApp.Models
+﻿using System;
+using System.Data;
+
+namespace BookRentalApp.Models
 {
     public class Loan
     {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
