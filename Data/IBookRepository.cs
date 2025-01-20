34da0f5b9b264a9f8871093098537f90d9d0470f
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:44:00 +0100

    napisalem kod dla pliku IBookRepository.cs

--- Data/IBookRepository.cs
+++ Data/IBookRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace BookRentalApp.Data
+﻿using System.Collections.Generic;
+using BookRentalApp.Models;
+
+namespace BookRentalApp.Data
 {
     public interface IBookRepository
     {
+        IEnumerable<Book> GetAll();
+        Book GetById (int id);
+        void Add(Book book);
+        void Update(Book book);
+        void Delete(int id);
     }
 }
