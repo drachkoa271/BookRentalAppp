c71467497da61643ac48cda8c2bcd7f7aa9e5339
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:31:23 +0100

    napisalem kod dla pliku User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,11 @@
-﻿namespace BookRentalApp.Models
+﻿using System;
+
+namespace BookRentalApp.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
