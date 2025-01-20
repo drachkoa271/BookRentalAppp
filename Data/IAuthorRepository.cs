46bad78ce6d6eb49b3bf262e96aa400509cb03d1
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:36:02 +0100

    napisalem kod dla pliku IAuthorRepository.cs

--- Data/IAuthorRepository.cs
+++ Data/IAuthorRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace BookRentalApp.Data
+﻿using System.Collections.Generic;
+using BookRentalApp.Models;
+
+namespace BookRentalApp.Data
 {
     public interface IAuthorRepository
     {
+        IEnumerable<Author> GetAll();
+        Author GetById(int id);
+        void Add(Author author);
+        void Update(Author author);
+        void Delete(Author author);
     }
 }
