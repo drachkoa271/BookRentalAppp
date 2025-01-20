d8563026c5b025b68ebc8068c9b3c1bd24ea55f1
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 22:31:59 +0100

    napisalem kod dla pliku UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,84 @@
-﻿namespace BookRentalApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using BookRentalApp.Models;
+using BookRentalApp.Data;
+
+namespace BookRentalApp.Controllers
 {
-    public class UsersController
+    public class UsersController : Controller
     {
+        private readonly IUserRepository _repository;
+        public UsersController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+        public IActionResult Indedx()
+        {
+            var users = _repository.GetAll();
+            return View(users);
+        }
+        public IActionResult Details(int id)
+        {
+            var user = _repository.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.Add(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+        public IActionResult Edit(int id)
+        {
+            var user = _repository.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _repository.Update(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+        public IActionResult Delete(int id)
+        {
+            var user = _repository.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _repository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
