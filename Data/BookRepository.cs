930bfa1d2ecda8da1d8719cfe8a0dd67e9b22946
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 22:05:26 +0100

    napisalem kod dla pliku BookRepository.cs

--- Data/BookRepository.cs
+++ Data/BookRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace BookRentalApp.Data
+﻿using System.Collections.Generic;
+using System.Linq;
+using BookRentalApp.Models;
+
+namespace BookRentalApp.Data
 {
-    public class BookRepository
+    public class BookRepository : IBookRepository
     {
+        private readonly ApplicationDbContext _context;
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Book> GetAll()
+        {
+            return _context.Books.ToList();
+        }
+        public Book GetById(int id)
+        {
+            return _context.Books.Find(id);
+        }
+        public void Add(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+        public void Update(Book book)
+        {
+            _context.Books.Update(book);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+        }
     }
 }
