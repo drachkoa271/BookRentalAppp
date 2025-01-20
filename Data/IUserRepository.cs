3be07ec3080f0c18a254bd252e52e66e776895a6
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:50:01 +0100

    napisalem kod dla pliku IUserRepository.cs

--- Data/IUserRepository.cs
+++ Data/IUserRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace BookRentalApp.Data
+﻿using System.Collections.Generic;
+using BookRentalApp.Models;
+
+namespace BookRentalApp.Data
 {
     public interface IUserRepository
     {
+        IEnumerable<User> GetAll();
+        User GetById (int id);
+        void Add (User user);
+        void Update (User user);
+        void Delete (int id);
     }
 }
