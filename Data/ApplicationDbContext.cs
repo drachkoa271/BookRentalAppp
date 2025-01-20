b29c0b6b1d001f2fdfb9c7d9ce750caa38863cd4
drachkoa271 <drachkoa271@gmail.com>
2025-01-20 21:22:14 +0100

    napisalem kod dla application

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace BookRentalApp.Data
+﻿using Microsoft.EntityFrameworkCore;
+using BookRentalApp.Models;
+
+namespace BookRentalApp.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
